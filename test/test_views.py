957fcbf1ad9ff9c12fd242badb830d02b32048c0
Ryan Conrad <rmconrad93@gmail.com>
2017-03-09 20:13:59 -0500

    templates: add templates, add test folder, adjust index

---
+++ test/test_views.py
@@ -0,0 +1,14 @@
+# Mock test data for views
+
+# def index():
+#     user = {'nickname': 'Fake User Name'}
+#     post = [
+#         {
+#             'author': {'nickname': 'Fake Userina'}
+#             'body': 'It rained all fucking day today.'
+#         },
+#         {
+#             'author': {'nickname': 'Fake User 2'}
+#             'body': 'Today was slightly better.'
+#         }
+#     ]
