6dfa4d70c2d276d4b22d4124d746e8307681798a
rmconrad <rmconrad93@gmail.com>
2017-03-21 18:18:12 -0400

    Templates (#1)
    * templates: add templates, add test folder, adjust index
    
    * add base template for inheritance

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Fake User Name'}
+    posts = [
+        {
+            'author': {'nickname': 'Fake Userina'},
+            'body': 'It rained all fucking day today.'
+        },
+        {
+            'author': {'nickname': 'Fake User 2'},
+            'body': 'Today was slightly better.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
