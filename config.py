4f5ece7e10cde3acfef29bdcc15db0e6d1d848ea
Ryan Conrad <rmconrad93@gmail.com>
2017-04-27 19:44:23 -0400

    initial setup for db and models

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'Secret_Key'
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 OPENID_PROVIDERS = [
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
