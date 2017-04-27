187d4f635aaec6659669df9c7b756f4d7dbdf4a2
rmconrad <rmconrad93@gmail.com>
2017-04-27 18:43:12 -0400

    Web forms (#2)
    * add form and login template
    
    * add new routes for login
    
    * add login validation
    
    * add openid providers list and make functional

--- app/views.py
+++ app/views.py
@@ -1,7 +1,27 @@
-from flask import render_template
+# imports
+from flask import render_template, flash, redirect
 from app import app
 
-@app.route('/')
+# import class
+from .forms import LoginForm
+
+# index view function
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+            (form.openid.data, str(form.remember_me.data))
+        )
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form,
+            providers=app.config['OPENID_PROVIDERS'])
+
+
+
+
+
+# old version - if removed index page breaks.
 @app.route('/index')
 def index():
     user = {'nickname': 'Fake User Name'}
