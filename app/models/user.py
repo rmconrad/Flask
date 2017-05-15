4ac559dbd6fc0caebf5744f02f11c660eab70be3
Ryan Conrad <rmconrad93@gmail.com>
2017-05-15 19:01:24 -0400

    split models.py into folder, create v2 of db

--- app/models/user.py
+++ app/models/user.py
@@ -4,6 +4,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % (self.username)
